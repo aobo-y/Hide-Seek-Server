b111256bb379d3eb8dd6dd14b474594b794262c9
Aobo Yang <yangaobo@gmail.com>
2018-10-18 14:17:34 -0400

    restruct the servlet

--- src/main/java/Servlet.java
+++ src/main/java/Servlet.java
@@ -58,159 +58,6 @@ public class Servlet extends HttpServlet {
         IAP.loadIDFRecord(idfPath);
     }
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request  servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException      if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("application/json;charset=UTF-8");
-        if (request.getMethod().equals("GET")) {
-
-        } else if (request.getMethod().equals("POST")) {
-            // get parameters from POST request
-            // timestamp is generated here
-            Date dNow = new Date();
-            String time = Util.convertDateToString(dNow);
-
-            String action = request.getParameter("action");
-            if (action.equals("R")) {
-                // register user
-                String uid = request.getParameter("uid");
-                JDBC.registerUser(uid);
-            } else if (action.equals("U")) {
-                System.out.println("Action is RE-RANK::::::");
-
-                // re-rank according to sinppets received
-                String[] jsonData = request.getParameterValues("json[]");
-                String uid = request.getParameter("uid");
-                String userProfile = JDBC.getProfile(uid);
-
-                Integer[] arr = IAP.reRankDocuments(jsonData, userProfile);
-                List<Integer> intList = Arrays.asList(arr);
-                String json = new Gson().toJson(intList);
-                response.getWriter().write(json);
-                System.out.println("::::::RE-RANK SUCCESS");
-            } else if (action.equals("UC")) {
-                System.out.println("Action is USER CLICK::::::");
-                String uid = request.getParameter("uid");
-                String snippet = request.getParameter("snip");
-                String query = request.getParameter("query");
-                int idx = Integer.valueOf(request.getParameter("click"));
-                String url = request.getParameter("url");
-                String title = request.getParameter("content");
-
-                // save user clicks
-                JDBC.saveClick(uid, url, title, query, 1, idx, time);
-                // update user profile using click
-                String profile = JDBC.getProfile(uid);
-                String newProfile = IAP.updateProfileUsingClick(snippet, profile);
-                JDBC.saveProfile(uid, newProfile);
-                System.out.println("::::::USER CLICK SUCCESS");
-            } else if (action.equals("SC")) {
-                System.out.println("Action is SIMULATED CLICK::::::");
-                String uid = request.getParameter("uid");
-                String query = request.getParameter("query");
-                int idx = Integer.valueOf(request.getParameter("click"));
-                String url = request.getParameter("url");
-                String title = request.getParameter("content");
-
-                // save simulated clicks
-                JDBC.saveClick(uid, url, title, query, 0, idx, time);
-                System.out.println("::::::SIMULATED CLICK SUCCESS");
-            } else if (action.equals("Q")) {
-
-                System.out.println("Action is QUERY::::::");
-
-                String uid = request.getParameter("uid");
-                String query = request.getParameter("query");
-                int numCover = Integer.parseInt(request.getParameter("numcover")) - 1;
-                Query curQuery = IAP.getQueryTopic(query);
-                ArrayList<Query> coverQueries;
-                int sessionNo = 0;
-                int actionNo = 0;
-                String sentToPython = "null";
-                String pythonQuery = null;
-                Query pQuery = new Query();
-                Map<String, String> map = new LinkedHashMap<>();
-
-                // get java cover queries
-                if (JDBC.getPreviousCoverQueryData(uid) == null) {
-                    // first query ever
-                    coverQueries = IAP.getCoverQueries(curQuery, numCover);
-                    //
-                } else {
-                    // exists action before
-                    QueryData qd = JDBC.getPreviousCoverQueryData(uid);
-                    actionNo = qd.getActionID() + 1;
-                    Query prevQuery = qd.getUserQuery();
-                    Date previousTime = Util.convertStringToDate(qd.getTime());
-                    if (Util.checkSameSession_time(previousTime, dNow)) {
-                        // same session
-                        sessionNo = qd.getSessionID();
-                        int sequentialEdited = IAP.checkSequentialEdited(curQuery, prevQuery);
-                        if (sequentialEdited == 0) {
-                            coverQueries = IAP.getCoverQueries(curQuery, numCover);
-                        } else {
-                            coverQueries = IAP.getCoverQueriesIfSeqEdited(curQuery, qd.getCoverQueryList(), numCover,
-                                    sequentialEdited);
-                        }
-                        sentToPython = qd.getPythonQuery().getQueryText();
-                    } else {
-                        // different session
-                        coverQueries = IAP.getCoverQueries(curQuery, numCover);
-                        sessionNo = qd.getSessionID() + 1;
-                    }
-                }
-
-                // get query from python program
-                try {
-                    pythonQuery = JDBC.getCover(sentToPython);
-                    pQuery = IAP.getQueryTopic(pythonQuery);
-                } catch (Exception ex) {
-                    Logger.getLogger(Servlet.class.getName()).log(Level.SEVERE, null, ex);
-                }
-
-                // respond to users as soon as possible
-                try {
-                    for (Query q : coverQueries) {
-                        map.put(q.getQueryText(), q.getQueryTopic());
-                    }
-                } catch (NullPointerException e) {
-                    System.out.println("    No java queries generated");
-                }
-                if (null != curQuery.getQueryTopic()) {
-                    map.put("input", curQuery.getQueryTopic());
-                }
-                if (null != pQuery.getQueryTopic()) {
-                    map.put(pQuery.getQueryText(), pQuery.getQueryTopic());
-                }
-                String json = new Gson().toJson(map);
-                response.getWriter().write(json);
-
-                // save query to database
-                if (null != curQuery.getQueryTopic()) {
-                    JDBC.saveQuery(curQuery, actionNo, sessionNo, 0, uid, time);
-                    for (Query q : coverQueries) {
-                        JDBC.saveQuery(q, actionNo, sessionNo, 1, uid, time);
-                    }
-                    JDBC.saveQuery(pQuery, actionNo, sessionNo, 2, uid, time);
-                }
-
-                // update user profile using query
-                String profile = JDBC.getProfile(uid);
-                String newProfile = IAP.updateProfileUsingQuery(curQuery, profile);
-                JDBC.saveProfile(uid, newProfile);
-                System.out.println("::::::QUERY SUCCESS");
-            }
-        }
-    }
-
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the
     // + sign on the left to edit the code.">
     /**
@@ -225,7 +72,7 @@ public class Servlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
-        processRequest(request, response);
+        response.setContentType("application/json;charset=UTF-8");
     }
 
     /**
@@ -240,7 +87,143 @@ public class Servlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
-        processRequest(request, response);
+        response.setContentType("application/json;charset=UTF-8");
+
+        // get parameters from POST request
+        // timestamp is generated here
+        Date dNow = new Date();
+        String time = Util.convertDateToString(dNow);
+
+        String action = request.getParameter("action");
+        if (action.equals("R")) {
+            // register user
+            String uid = request.getParameter("uid");
+            JDBC.registerUser(uid);
+        } else if (action.equals("U")) {
+            System.out.println("Action is RE-RANK::::::");
+
+            // re-rank according to sinppets received
+            String[] jsonData = request.getParameterValues("json[]");
+            String uid = request.getParameter("uid");
+            String userProfile = JDBC.getProfile(uid);
+
+            Integer[] arr = IAP.reRankDocuments(jsonData, userProfile);
+            List<Integer> intList = Arrays.asList(arr);
+            String json = new Gson().toJson(intList);
+            response.getWriter().write(json);
+            System.out.println("::::::RE-RANK SUCCESS");
+        } else if (action.equals("UC")) {
+            System.out.println("Action is USER CLICK::::::");
+            String uid = request.getParameter("uid");
+            String snippet = request.getParameter("snip");
+            String query = request.getParameter("query");
+            int idx = Integer.valueOf(request.getParameter("click"));
+            String url = request.getParameter("url");
+            String title = request.getParameter("content");
+
+            // save user clicks
+            JDBC.saveClick(uid, url, title, query, 1, idx, time);
+            // update user profile using click
+            String profile = JDBC.getProfile(uid);
+            String newProfile = IAP.updateProfileUsingClick(snippet, profile);
+            JDBC.saveProfile(uid, newProfile);
+            System.out.println("::::::USER CLICK SUCCESS");
+        } else if (action.equals("SC")) {
+            System.out.println("Action is SIMULATED CLICK::::::");
+            String uid = request.getParameter("uid");
+            String query = request.getParameter("query");
+            int idx = Integer.valueOf(request.getParameter("click"));
+            String url = request.getParameter("url");
+            String title = request.getParameter("content");
+
+            // save simulated clicks
+            JDBC.saveClick(uid, url, title, query, 0, idx, time);
+            System.out.println("::::::SIMULATED CLICK SUCCESS");
+        } else if (action.equals("Q")) {
+
+            System.out.println("Action is QUERY::::::");
+
+            String uid = request.getParameter("uid");
+            String query = request.getParameter("query");
+            int numCover = Integer.parseInt(request.getParameter("numcover")) - 1;
+            Query curQuery = IAP.getQueryTopic(query);
+            ArrayList<Query> coverQueries;
+            int sessionNo = 0;
+            int actionNo = 0;
+            String sentToPython = "null";
+            String pythonQuery = null;
+            Query pQuery = new Query();
+            Map<String, String> map = new LinkedHashMap<>();
+
+            // get java cover queries
+            if (JDBC.getPreviousCoverQueryData(uid) == null) {
+                // first query ever
+                coverQueries = IAP.getCoverQueries(curQuery, numCover);
+                //
+            } else {
+                // exists action before
+                QueryData qd = JDBC.getPreviousCoverQueryData(uid);
+                actionNo = qd.getActionID() + 1;
+                Query prevQuery = qd.getUserQuery();
+                Date previousTime = Util.convertStringToDate(qd.getTime());
+                if (Util.checkSameSession_time(previousTime, dNow)) {
+                    // same session
+                    sessionNo = qd.getSessionID();
+                    int sequentialEdited = IAP.checkSequentialEdited(curQuery, prevQuery);
+                    if (sequentialEdited == 0) {
+                        coverQueries = IAP.getCoverQueries(curQuery, numCover);
+                    } else {
+                        coverQueries = IAP.getCoverQueriesIfSeqEdited(curQuery, qd.getCoverQueryList(), numCover,
+                                sequentialEdited);
+                    }
+                    sentToPython = qd.getPythonQuery().getQueryText();
+                } else {
+                    // different session
+                    coverQueries = IAP.getCoverQueries(curQuery, numCover);
+                    sessionNo = qd.getSessionID() + 1;
+                }
+            }
+
+            // get query from python program
+            try {
+                pythonQuery = JDBC.getCover(sentToPython);
+                pQuery = IAP.getQueryTopic(pythonQuery);
+            } catch (Exception ex) {
+                Logger.getLogger(Servlet.class.getName()).log(Level.SEVERE, null, ex);
+            }
+
+            // respond to users as soon as possible
+            try {
+                for (Query q : coverQueries) {
+                    map.put(q.getQueryText(), q.getQueryTopic());
+                }
+            } catch (NullPointerException e) {
+                System.out.println("    No java queries generated");
+            }
+            if (null != curQuery.getQueryTopic()) {
+                map.put("input", curQuery.getQueryTopic());
+            }
+            if (null != pQuery.getQueryTopic()) {
+                map.put(pQuery.getQueryText(), pQuery.getQueryTopic());
+            }
+            String json = new Gson().toJson(map);
+            response.getWriter().write(json);
+
+            // save query to database
+            if (null != curQuery.getQueryTopic()) {
+                JDBC.saveQuery(curQuery, actionNo, sessionNo, 0, uid, time);
+                for (Query q : coverQueries) {
+                    JDBC.saveQuery(q, actionNo, sessionNo, 1, uid, time);
+                }
+                JDBC.saveQuery(pQuery, actionNo, sessionNo, 2, uid, time);
+            }
+
+            // update user profile using query
+            String profile = JDBC.getProfile(uid);
+            String newProfile = IAP.updateProfileUsingQuery(curQuery, profile);
+            JDBC.saveProfile(uid, newProfile);
+            System.out.println("::::::QUERY SUCCESS");
+        }
     }
 
     /**
@@ -251,6 +234,5 @@ public class Servlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Short description";
-    }// </editor-fold>
-
+    }
 }
